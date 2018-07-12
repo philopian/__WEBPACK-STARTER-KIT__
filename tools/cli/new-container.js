4103648f6d785f1f89c88a7339cb4c9c3024c4f4
philopian <pdotwdot@gmail.com>
2018-07-11 21:00:08 -0700

    updated the CLI for new container's storybook stuff

--- tools/cli/new-container.js
+++ tools/cli/new-container.js
@@ -199,13 +199,10 @@ import { withInfo } from "@storybook/addon-info";
 import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 import { setConsoleOptions } from "@storybook/addon-console";
 
-const { Provider } = require("react-redux");
-const { createStore } = require("redux");
-const reducers = require("../../reducers").default;
-const store = createStore(reducers);
-
 import React from "react";
-import Component from "./index.js";
+import { ${opts.componentName.titleCase} as Component } from "./${
+    opts.componentName.titleCase
+  }.jsx";
 
 setConsoleOptions({
   panelExclude: [/[HMR]/]
@@ -214,10 +211,10 @@ setConsoleOptions({
 const Info = {
   componentSection: "Container",
   title: "${opts.componentName.titleCase}",
-  about: "${opts.componentDescription}",
-  props: {
-    xx: "Hello Container!",
-    aa: action("Action Clicked")
+  about: "this is a simple example container",
+  mockProps: {
+    // exampleProp: "a little sample data for you",
+    // exampleMethod: () => action("exampleMethod")
   }
 };
 
@@ -225,12 +222,9 @@ storiesOf(Info.componentSection, module)
   .addDecorator(withKnobs)
   .add(
     Info.title,
-    withInfo(Info.about)(() => (
-      <Provider store={store}>
-        <Component />
-      </Provider>
-    ))
-  );`;
+    withInfo(Info.about)(() => <Component {...Info.mockProps} />)
+  );
+`;
   makeFile(filepath, fileContents, fileName);
 };
 
